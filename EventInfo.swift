4998cb77c3d56c44d1e700e67d19b3fa08776db4
Carter Smith <carters3@illinois.edu>
2020-09-13 23:04:27 -0500

    GET /event/ retrieves decodes retrieved JSON info
    Utilizes structs, Event & EventList, to handle JSON data from /event/

---
+++ EventInfo.swift
@@ -0,0 +1,38 @@
+//
+//  EventInfo.swift
+//  HackIllinoisDemo
+//
+//  Created by Carter Smith on 9/13/20.
+//  Copyright © 2020 Carter Smith. All rights reserved.
+//
+
+import Foundation
+
+public struct Event: Codable {
+    let id: String
+    let name: String
+    let description: String
+    let startTime: Date
+    let endTime: Date
+    let locations: [String]
+    let sponsor: String
+    let eventType: String
+    
+    enum CodingKeys: String, CodingKey {
+        case id = "id"
+        case name = "name"
+        case description = "description"
+        case startTime = "startTime"
+        case endTime = "endTime"
+        case locations = "locations"
+        case sponsor = "sponsor"
+        case eventType = "eventType"
+    }
+    
+}
+
+public struct EventList: Decodable {
+    let events: [Event]
+
+}
+
--- HackIllinoisDemo/ViewController.swift
+++ HackIllinoisDemo/ViewController.swift
@@ -8,13 +8,56 @@
 
 import UIKit
 
+public let url = "https://api.hackillinois.org/event/"
+
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        getData(from: url)
     }
+    
+    /*
+    private func getData() {
+        let url = URL(string: "https://api.hackillinois.org/event/")!
+        
+        URLSession.shared.dataTask(with: url) {(data, response, error) in
+            
+            do {
 
-
+                print(EventArr.self)
+            }
+            catch
+            {
+                print("error: data cannot be found")
+            }
+            
+        }.resume()
+    } */
+    
+    
+    private func getData(from url: String) {
+        URLSession.shared.dataTask(with: URL(string: url)!, completionHandler: { data, response, error in
+            guard let data = data, error == nil else {
+                print("An error has occurred!")
+                return
+            }
+            
+            // Convert data to the struct
+            var eventList: EventList
+            do {
+                eventList = try JSONDecoder().decode(EventList.self,from: data)
+                print(eventList.events[0].name)
+            
+            }
+            catch {
+                print("failed to convert: \(error)")
+            }
+                        
+            }).resume()
+        
+    }
 }
+
 
